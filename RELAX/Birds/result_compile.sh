a3c5ba33938b94058ce2fa382f1c04d924ec6bb6
Ashu2195 <52165511+Ashu2195@users.noreply.github.com>
2022-03-29 13:53:44 +0530

    Add files via upload

---
+++ RELAX/Birds/result_compile.sh
@@ -0,0 +1,9 @@
+i=$1
+for d in `ls -1 *_relax`
+do
+pval=`grep "^Like" $d|awk '{print $6}'|sed 's/\*\*\.//g'`
+kval1=`grep "Relaxation/intensification" $d|awk '{print $6}'|head -1`
+test=`grep "_Test_ set:" $d|awk '{print $9}'|sed 's/\`//g'`
+back=`grep "_Reference_ set:" $d|cut -f2 -d ":"|sed 's/ //g'`
+echo $test $back $pval $kval1 >>"$i".Results.txt
+done
