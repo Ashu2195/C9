e6ae3fcf978cee582ba0b23c4689fb0ee4531b1f
Ashu2195 <52165511+Ashu2195@users.noreply.github.com>
2022-03-29 21:29:13 +0530

    Delete PAML_branch_model directory

--- PAML_branch_model/Chondrichthyes_Coelacanth/tree_script.r
+++
@@ -1,4 +0,0 @@
-library(ape)
-a<-read.tree("Chondrichthyes_Coelacanth_C9.nwk")
-b<-unroot(a)
-write.tree(b,"Chondrichthyes_Coelacanth_C9.nwk.tree")
--- PAML_branch_model/Crocodilia/tree_script.r
+++
@@ -1,4 +0,0 @@
-library(ape)
-a<-read.tree("Crocodilia_C9.nwk")
-b<-unroot(a)
-write.tree(b,"Crocodilia_C9.nwk.tree")
