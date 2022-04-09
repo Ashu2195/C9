546ae4022e16383f44ab94c4db4ce6c6cdf15404
Ashu2195 <52165511+Ashu2195@users.noreply.github.com>
2022-04-09 21:35:52 +0530

    Delete PAML_branch_model directory

--- PAML_branch_model/Birds/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in `ls splita*`
-do
-for j in `cat "$i"`
-do
-( codeml "$j" ) &
-done
-wait
-done
--- PAML_branch_model/Birds/tree_script.r
+++
@@ -1,4 +0,0 @@
-library(ape)
-a<-read.tree("Birds_C9.nwk")
-b<-unroot(a)
-write.tree(b,"Birds_C9.nwk.tree")
