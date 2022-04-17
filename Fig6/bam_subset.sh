b468f8aca89b62deb09f784c927684258561f590
Ashu2195 <52165511+Ashu2195@users.noreply.github.com>
2022-04-17 13:03:16 +0530

    Delete bam_subset.sh

--- Fig6/bam_subset.sh
+++
@@ -1,7 +0,0 @@
-#Subset bam files with the flank of dab2 and fyb
-samtools view -b SRR7062907.bam Z:12270224-12378919 > SRR7062907_C9_subsetted.bam
-#add read group in subseted bam files
-java -jar /home/sagar/PPP/bin/picard.jar AddOrReplaceReadGroups I=SRR7062907_C9_subsetted.bam O=SRR7062907_C9_subsetted_RG.bam SO=coordinate RGID=SRR7062907 RGLB=library RGPL=ILLUMINA RGPU=SRR7062907 RGSM=Gallus_gallus
-#merge all subseted
-samtools merge Gallus_gallus_C9_subsetted_RG.bam SRR7062907_C9_subsetted_RG.bam
-
